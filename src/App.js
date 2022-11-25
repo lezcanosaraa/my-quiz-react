ade66b3e3349f9e6457595c2e0a636a2f554fe0b
Sara Lezcano <sara.lezcano@pushon.co.uk>
2022-11-25 15:52:22 +0000

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { questions } from "./questions.js";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showScore ? (
+        <div className="showScore">
+          <h1>
+            Your score is {score} out of {questions.length}
+          </h1>
+        </div>
+      ) : (
+        <>
+          <section className="question-wrap">
+            <h1>
+              Question {currentQuestion + 1}/{questions.length}
+            </h1>
+            <p>{questions[currentQuestion].questionText}</p>
+            <ul>
+              {questions[currentQuestion].answerOptions.map((answerText) => (
+                <li
+                  onClick={() => optionClicked(answerText.isCorrect)}
+                  key={answerText.id}
+                >
+                  {answerText.answerText}
+                </li>
+              ))}
+            </ul>
+          </section>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/questions.js
@@ -0,0 +1,79 @@
+export const questions = [
+  {
+    questionText: "What is 4 + 4?",
+    answerOptions: [
+      { id:0, answerText: "8", isCorrect: true },
+      { id:1, answerText: "2", isCorrect: false },
+      { id:2, answerText: "3", isCorrect: false },
+      { id:3, answerText: "5", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How many minutes are in an hour?",
+    answerOptions: [
+      { id:0, answerText: "800", isCorrect: false },
+      { id:1, answerText: "12", isCorrect: false },
+      { id:2, answerText: "60", isCorrect: true },
+      { id:3, answerText: "500", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the Capital of England?",
+    answerOptions: [
+      { id:0, answerText: "Madrid", isCorrect: false },
+      { id:1, answerText: "London", isCorrect: true },
+      { id:2, answerText: "Dublin", isCorrect: false },
+      { id:3, answerText: "Bejing", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    questionText: "How many corners does a square have?",
+    answerOptions: [
+      { id:0, answerText: "4", isCorrect: true },
+      { id:1, answerText: "3", isCorrect: false },
+      { id:2, answerText: "2", isCorrect: false },
+      { id:3, answerText: "10", isCorrect: false },
+    ],
+  },
+  {
+    id:4,
+    questionText: "Who wrote the Harry Potter book series?",
+    answerOptions: [
+      { id:0, answerText: "Ron Weasley", isCorrect: false },
+      { id:1, answerText: "Tom Riddle", isCorrect: false },
+      { id:2, answerText: "Jane Austen", isCorrect: false },
+      { id:3, answerText: "J. K. Rowling", isCorrect: true },
+    ],
+  },
+  {
+    id: 5,
+    questionText: "Who was Agnes Gonxha Bojaxhiu better known as?",
+    answerOptions: [
+      { id:0, answerText: "Mother Theresa", isCorrect: true },
+      { id:1, answerText: "Queen Elizabeth II", isCorrect: false },
+      { id:2, answerText: "Joan of Arc", isCorrect: false },
+      { id:3, answerText: "Beyoncé", isCorrect: false },
+    ],
+  },
+  {
+    id: 6,
+    questionText: "How many films did Pierce Brosnan play James Bond??",
+    answerOptions: [
+      { id:0, answerText: "2", isCorrect: false },
+      { id:1, answerText: "10", isCorrect: false },
+      { id:2, answerText: "3", isCorrect: false },
+      { id:3, answerText: "4", isCorrect: true },
+    ],
+  },
+  {
+    id: 7,
+    questionText: "What country has the highest population?",
+    answerOptions: [
+      {  id:0, answerText: "Canada", isCorrect: false },
+      {  id:1, answerText: "Russia", isCorrect: false },
+      {  id:2, answerText: "India", isCorrect: false },
+      {  id:3, answerText: "China", isCorrect: true },
+    ],
+  },
+];
